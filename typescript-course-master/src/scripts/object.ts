bf99f0deb8f12ba9c864d486094017049f3bb888
huy <danghuy9229@gmail.com>
2024-07-04 17:48:53 +0700

    commit video 3

--- typescript-course-master/src/scripts/object.ts
+++ typescript-course-master/src/scripts/object.ts
@@ -5,3 +5,15 @@ const student: {
   name: "evondev",
   age: 29,
 };
+
+const another: {
+  myName: string;
+  age: number;
+  school?: string;
+  score?: number
+} = {
+  myName: "huy",
+  age: 29,
+  school: "RMIT",
+  score: 100
+}
